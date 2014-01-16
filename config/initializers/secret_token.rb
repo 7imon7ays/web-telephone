29e2b45a5e4ef2742fbf6eee9fadecb329776e3a
Simon Chaffetz <simis1000@gmail.com>
2014-01-16 15:37:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebTelephone::Application.config.secret_key_base = '73d053c98fb9d93ac3a084eb5864bb210f0263fc85d3730dc55ce5172f8d5a470f14d5082c46a440350d79c8ae6849c321664c2e661c866ad9ebb262831fbea0'
