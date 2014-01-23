636393e1b293faf05b6a2ad89661b360707a34f6
Will Moritz <will@crowdfavorite.com>
2014-01-22 20:04:47 -0700

    spellchecking

--- app/assets/javascripts/contributions.js
+++ app/assets/javascripts/contributions.js
@@ -2,6 +2,7 @@ var WebTelephone = {}
 
 jQuery(document).ready(function($) {
 	var $canvasWrapper = $("#canvas-wrapper");
+	console.log($canvasWrapper);
 	new WebTelephone.CanvasDrawer($canvasWrapper).init();
     new WebTelephone.FormHandler($canvasWrapper).listenForSubmission()
 });
