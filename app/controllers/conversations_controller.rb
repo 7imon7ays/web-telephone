3043e73102230f82b9747bb54752ebdeea4a2c14
Simon Chaffetz <simis1000@gmail.com>
2014-02-01 22:19:34 -0800

    Render bootstrapped thread in thank you page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,6 +2,5 @@ class ConversationsController < ApplicationController
   def show
     thread_id = params[:key].match(/\d/).to_s.to_i
     @thread = Conversation.find(thread_id)
-    render json: @thread
   end
 end
