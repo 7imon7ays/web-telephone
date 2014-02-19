1ad8cc40f6c530af4d8606004d3289de9dd6c273
Will Moritz <will@crowdfavorite.com>
2014-02-18 20:13:41 -0700

    removed some old stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  unless  ActionController::Base.consider_all_requests_local
-    rescue_from Exception, :with => :render_404
-  end
-
-  private
-
-    def render_404
-      render :template => 'error_pages/404', :layout => false, :status => :not_found
-    end
-
 end
 
 
