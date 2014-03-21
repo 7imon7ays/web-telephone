42d503247318b6bc1b8b935a3d69a3659089edc3
Will Moritz <will@crowdfavorite.com>
2014-03-21 16:50:13 -0500

    updated notification email

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -1,11 +1,11 @@
 class PlayerMailer < ActionMailer::Base
-  default from: "thetelephoneoperators@gmail.com"
+  default from: "TheTelephoneOperators@gmail.com"
 
   def new_child_email(emailer, new_child)
     @emailer = emailer
     @contribution = emailer.contribution
     @new_child = new_child
-    mail(to: emailer.address, subject: "Your voice was heard!")
+    mail(to: emailer.address, subject: "Someone answered your call!")
   end
 end
 
